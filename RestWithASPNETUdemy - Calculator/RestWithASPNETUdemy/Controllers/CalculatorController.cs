53f17ca72512faa1eb6204ac01211810d1161c6a
Alexandre Chicrala <alexandre.chicrala@lojasrede.com.br>
2020-09-15 12:52:58 -0300

    Implementacao do metodo de soma e criacao do novo Controller

---
+++ RestWithASPNETUdemy - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+
+        // GET api/values/5/5
+        [HttpGet("{firstnumber}/{secondnumber}")]
+        public IActionResult Sum(string firstnumber, string secondnumber)
+        {
+            try
+            {
+                var total = Convert.ToDecimal(firstnumber) + Convert.ToDecimal(secondnumber);
+                return Ok(total.ToString());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Invalid Input");
+            }
+
+        }
+
+
+    }
+}
