94cc93ad20a5267fff7c32ac4e50f22dc070496b
Alexandre Chicrala <alexandre.chicrala@lojasrede.com.br>
2020-09-22 12:47:31 -0300

    Acesso a dados Mysql

--- RestWithASPNETUdemy - Using different verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
+++ RestWithASPNETUdemy - Using different verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
 
 namespace RestWithASPNETUdemy.Controllers
 {
@@ -11,37 +13,58 @@ namespace RestWithASPNETUdemy.Controllers
     public class PersonsController : ControllerBase
     {
 
+        private IPersonInterface _personservice;
+
+        public PersonsController(IPersonInterface personservice)
+        {
+            _personservice = personservice;
+        }
 
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_personservice.FindAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult Get(long id)
         {
-            return "value";
+            var person = _personservice.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(person);
+            }
+                
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Person person)
         {
+            if (person == null) BadRequest();
+            return new ObjectResult(_personservice.Create(person));
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put([FromBody] Person person)
         {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personservice.Update(person));
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(long id)
         {
+            _personservice.Delete(id);
+            return NoContent();
         }
 
 
---
+++ RestWithASPNETUdemy - Using different verbs/RestWithASPNETUdemy/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonInterface
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Leandro",
+                LastName = "Costa",
+                Address = "Uberlândia - MG",
+                Gender = "Male"
+            };
+            
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = i,
+                FirstName = "Geraldo",
+                LastName = "Magela",
+                Address = "Uberlândia - MG",
+                Gender = "Male"
+            };
+        }
+
+    }
+
+}
