629ea3fae9af74c6e1ca4d2b8ab2b75887f00f39
Alexandre Chicrala <alexandre.chicrala@lojasrede.com.br>
2020-10-07 13:02:48 -0300

    Repositório genérico

---
+++ RestWithASPNETUdemy - Using different verbs/RestWithASPNETUdemy/Model/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string AUTHOR { get; set; }
+        public string TITLE { get; set; }
+    }
+}
---
+++ RestWithASPNETUdemy - Using different verbs/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Model.Base;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        private MySqlContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySqlContext context)
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
